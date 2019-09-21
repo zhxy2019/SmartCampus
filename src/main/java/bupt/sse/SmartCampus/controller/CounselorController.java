d9a065203c0de074a143dce31c097aeaa4c4a3b0
XipengCao <347929599@qq.com>
2019-09-21 15:33:48 +0800

    辅导员controller

---
+++ src/main/java/bupt/sse/SmartCampus/controller/CounselorController.java
@@ -0,0 +1,9 @@
+package bupt.sse.SmartCampus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/counselor")
+public class CounselorController {
+}
