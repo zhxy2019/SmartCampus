d5a851b790c4949d66ef951cf554c11667da5ff6
Xstephen <347929599@qq.com>
2019-10-24 16:06:34 +0800

    整合辅导员功能

--- src/main/java/bupt/sse/SmartCampus/controller/StudyProfileController.java
+++ src/main/java/bupt/sse/SmartCampus/controller/StudyProfileController.java
@@ -6,7 +6,6 @@ import bupt.sse.SmartCampus.service.*;
 import bupt.sse.SmartCampus.utils.Message;
 import bupt.sse.SmartCampus.utils.PageBean;
 import net.sf.json.JSONObject;
-import org.omg.PortableInterceptor.INACTIVE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
